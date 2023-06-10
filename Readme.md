e38b267f0e0f28d717f2aab37a243819b0230696
Jeff Mark <jeffmarkjoseph@gmail.com>
2023-06-10 23:33:26 +0530

    Added Layer Control Panel

--- Readme.md
+++ Readme.md
@@ -5,6 +5,5 @@ This repo contains my experiments with folium. The code for this project is writ
 I used markers in the map by marking different locations across my college -RVCE.
 Data is taken from a csv file which contains information related to a particular location's name, type, latitude, longtitude and status.
 Different coloured markers are generated depending on the type of building or area.
-All areas under construction are marked with a red marker.
-
-The other part was adding a map layer that showed the population of each country by setting limits. Darker the colour- more the population of the country.
+All areas under construction are marked with a red marker. The other part was adding a map layer that showed the population of each country by setting limits. Darker the colour- more the population of the country.
+A layer control panel enables the toggling between the different layers of the map
--- Web Map.py
+++ Web Map.py
@@ -8,9 +8,10 @@ type = list(data["Type"])
 names = list(data["Name"])
 status = list(data["Status"])
 
-map = folium.Map(location=[12.923177868020359, 77.49928786068105], zoom_start = 20)
+map = folium.Map(location=[12.923177868020359, 77.49928786068105], zoom_start = 18)
 
-fg = folium.FeatureGroup(name="RVCE")
+fg1 = folium.FeatureGroup(name="RVCE")
+fg2 = folium.FeatureGroup(name="Population")
 
 for latt,long,ty,name,state in zip(lat, lon, type, names, status):
     if ty == "Department":
@@ -31,11 +32,16 @@ for latt,long,ty,name,state in zip(lat, lon, type, names, status):
         name= str(name)+" (Under Construction)"
     if ty==" ":
         ty="Click Me!"
-    fg.add_child(folium.Marker(location=(latt, long), popup=name, icon=folium.Icon(color=colour), tooltip=ty))
+    fg1.add_child(folium.Marker(location=(latt, long), popup=name, icon=folium.Icon(color=colour), tooltip=ty))
+    #Without a feature group, each marker will be added to a different layer when the above add_child executes
 
-fg.add_child(folium.GeoJson(data=open('Applications\App1\world.json', 'r', encoding='utf-8-sig').read(),
+fg2.add_child(folium.GeoJson(data=open('Applications\App1\world.json', 'r', encoding='utf-8-sig').read(),
                              style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 50000000 
                                                        else 'orange' if x['properties']['POP2005'] < 100000000 else 'red'}))
 
-map.add_child(fg)
+
+map.add_child(fg1)
+map.add_child(fg2)
+map.add_child(folium.LayerControl())
+
 map.save("Applications\App1\Map1.html")
