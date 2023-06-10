e341025f1641c9a66e476fe2b8c31b093588b479
Jeff Mark <jeffmarkjoseph@gmail.com>
2023-06-10 20:12:50 +0530

    Removed unnecessary comments

--- Web Map.py
+++ Web Map.py
@@ -32,9 +32,7 @@ for latt,long,ty,name,state in zip(lat, lon, type, names, status):
     if ty==" ":
         ty="Click Me!"
     fg.add_child(folium.Marker(location=(latt, long), popup=name, icon=folium.Icon(color=colour), tooltip=ty))
-    #fg.add_child(folium.CircleMarker(location=(latt, long), popup=name, radius =5, fill_color=colour, tooltip=ty, fill_opacity=0.8))
-
-#fg.add_child(folium.GeoJson)
+    
 
 map.add_child(fg)
 map.save("Applications\App1\Map1.html")
